f9012d7d40f57f0bc87d6ba7fe9261a766133b71
hujh <whowhopipi@163.com>
2017-01-11 10:52:34 +0800

    添加POI功能

---
+++ xframework-boot/poi/src/main/java/com/xframework/boot/poi/XframeworkPoiAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.xframework.boot.poi;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class XframeworkPoiAutoConfiguration {
+
+	
+}
