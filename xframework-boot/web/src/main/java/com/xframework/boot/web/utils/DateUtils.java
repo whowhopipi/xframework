45bd0c5cb88e2f8cd2b8b552f13c7076d5e26940
hujh <whowhopipi@163.com>
2017-01-12 14:39:29 +0800

    调整日期转换线程安全问题

--- xframework-boot/web/src/main/java/com/xframework/boot/web/utils/DateUtils.java
+++ xframework-boot/web/src/main/java/com/xframework/boot/web/utils/DateUtils.java
@@ -10,10 +10,10 @@ public class DateUtils {
 
 	final private static Object LOCK = new Object();
 
-	private static Map<String, SimpleDateFormat> sdfs = new HashMap<>();
-
-	private static SimpleDateFormat getSdf(String pattern) {
-		SimpleDateFormat sdf = sdfs.get(pattern);
+	private static Map<String, ThreadLocal<SimpleDateFormat>> sdfs = new HashMap<>();
+    
+	private static ThreadLocal<SimpleDateFormat> getSdf(String pattern) {
+		ThreadLocal<SimpleDateFormat> sdf = sdfs.get(pattern);
 
 		if (sdf != null)
 			return sdf;
@@ -24,7 +24,12 @@ public class DateUtils {
 			if (sdf != null)
 				return sdf;
 
-			sdf = new SimpleDateFormat(pattern);
+			sdf = new ThreadLocal<SimpleDateFormat>() {
+				@Override
+				protected SimpleDateFormat initialValue() {
+					return new SimpleDateFormat(pattern);
+				}
+			};
 			sdfs.put(pattern, sdf);
 
 			return sdf;
@@ -32,12 +37,12 @@ public class DateUtils {
 	}
 
 	public static String formate(String pattern, Date date) {
-		SimpleDateFormat sdf = getSdf(pattern);
+		SimpleDateFormat sdf = getSdf(pattern).get();
 		return sdf.format(date);
 	}
 
 	public static Date parse(String pattern, String dataStr) throws ParseException {
-		SimpleDateFormat sdf = getSdf(pattern);
+		SimpleDateFormat sdf = getSdf(pattern).get();
 		return sdf.parse(dataStr);
 	}
 }
