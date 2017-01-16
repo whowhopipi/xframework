20b9dd98b877e5d6b245b596553480ca847916ea
hujh <whowhopipi@163.com>
2017-01-16 15:34:35 +0800

    优化Mybatis配置

--- xframework-boot/mybatis/src/test/java/com/xframework/boot/ibator/test/mapper/test/SystemParamMapperTest.java
+++ xframework-boot/mybatis/src/test/java/com/xframework/boot/ibator/test/mapper/test/SystemParamMapperTest.java
@@ -11,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.PageRowBounds;
 import com.xframework.boot.ibator.test.XframeworkMybatisTest;
 import com.xframework.boot.ibator.test.entity.SysParam;
 import com.xframework.boot.ibator.test.entity.SysParamExample;
@@ -31,30 +34,45 @@ public class SystemParamMapperTest {
 
 	@Before
 	public void before() {
-//		sysParamMapper = sqlSession.getMapper(SysParamMapper.class);
+		// sysParamMapper = sqlSession.getMapper(SysParamMapper.class);
 	}
 
 	@Test
 	public void testInsert() {
-		SysParam sysParam = new SysParam();
+		for (int i = 0; i < 50; i++) {
+			SysParam sysParam = new SysParam();
 
-		sysParam.setParamCode("XXX2");
-		sysParam.setParamValue("XXX");
-		sysParam.setParamDesc("XXX");
+			sysParam.setParamCode("code:" + i);
+			sysParam.setParamValue("value:" + i);
+			sysParam.setParamDesc("desc:" + i);
 
-		int ret = sysParamMapper.insert(sysParam);
-		log.debug("插入，返回值：" + ret);
+			int ret = sysParamMapper.insert(sysParam);
+			log.debug("插入，返回值：" + ret);
+		}
 	}
 
 	@Test
 	public void testSelect() {
 		SysParamExample example = new SysParamExample();
+		example.setOrderByClause("param_code");
 		long total = sysParamMapper.countByExample(example);
 		log.debug("total = " + total);
 		System.out.println("total = " + total);
-		
-		List<SysParam> params = sysParamMapper.selectByExample(example);
+
+		PageRowBounds rowBounds = new PageRowBounds(2, 10);
+		List<SysParam> params = sysParamMapper.selectByExampleWithRowbounds(example, rowBounds);
+		System.out.println("总共有:" + rowBounds.getTotal() + "条记录");
+		for (SysParam param : params) {
+			System.out.println(param);
+		}
 		log.debug(params.toString());
 		System.out.println(params.toString());
+
+		PageInfo<SysParam> pages = PageHelper.startPage(0, 5)
+				.doSelectPageInfo(() -> sysParamMapper.selectByExample(example));
+		System.out.println("总共有：" + pages.getTotal());
+		for (SysParam param : pages.getList()) {
+			System.out.println(param);
+		}
 	}
 }
