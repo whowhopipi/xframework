9950f8fdfa22f6d7e327f78913d8faf95f97e7ea
hujh <whowhopipi@163.com>
2017-01-20 13:17:48 +0800

    增加data属性支持

--- xframework-boot/web/src/main/java/com/xframework/boot/web/XframeworkWebConfiguration.java
+++ xframework-boot/web/src/main/java/com/xframework/boot/web/XframeworkWebConfiguration.java
@@ -1,10 +1,13 @@
 package com.xframework.boot.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
 
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
 import com.xframework.boot.web.service.ApplicationVersionService;
 import com.xframework.boot.web.service.impl.ApplicationVersionServiceImpl;
 
@@ -18,5 +21,11 @@ public class XframeworkWebConfiguration {
 	@ConditionalOnMissingBean(ApplicationVersionService.class)
 	public ApplicationVersionServiceImpl getApplication() {
 		return new ApplicationVersionServiceImpl(currentVersion);
+	}
+
+	@Bean
+	@ConditionalOnBean(TemplateEngine.class)
+	public DataAttributeDialect getApplication(TemplateEngine engine) {
+		return new DataAttributeDialect();
 	}
 }
