ceaafa8d2b915dad58340c11bdd897c8fd5d3549
hujh <whowhopipi@163.com>
2017-01-20 17:31:37 +0800

    sync

--- xframework-boot/web/src/main/java/com/xframework/boot/web/XframeworkWebConfiguration.java
+++ xframework-boot/web/src/main/java/com/xframework/boot/web/XframeworkWebConfiguration.java
@@ -25,6 +25,7 @@ public class XframeworkWebConfiguration {
 
 	@Bean
 	@ConditionalOnBean(TemplateEngine.class)
+	@ConditionalOnMissingBean
 	public DataAttributeDialect getApplication(TemplateEngine engine) {
 		return new DataAttributeDialect();
 	}
